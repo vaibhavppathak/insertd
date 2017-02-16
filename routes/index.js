d95e37f9009b1291ec914422ac05f3fa494c3f0e
vaibhavpathak1994 <vaibhav_pathak@excellencetechnologies.in>
2017-02-16 15:12:37 +0530

    changes in these all files

--- routes/index.js
+++ routes/index.js
@@ -1,49 +1,43 @@
-var express = require('express'); // require express module
-var app = express(); // creatig insatnce of express function 
+var express = require('express'); //require express module
+var app = express(); //creatig insatnce of express function 
 var router = express.Router();
 
 <!--- insert data into mongodb ---->
 
-router.all('/users/create', function (req, res) {
+router.all('/users/create', function(req, res) {
     var name = req.body.name;
     var email = req.body.email;
     var password = req.body.password;
- if(name.length > 0 && email.length > 0 && password.length > 0){
-       var record = new req.Collection_user({
-          "name": name,
-          "email": email,
-          "password":password,
+    if (name.length > 0 && email.length > 0 && password.length > 0) {
+        var record = new req.Collection_user({
+            "name": name,
+            "email": email,
+            "password": password,
         });
-       record.save(function (err) {
-          if(err){
-               throw error;
+        record.save(function(err) {
+            if (err) {
+                throw error;
             } else {
-              console.log("record is inserted successfully");
-              res.send("Record inserted successfully");
+                res.send("Record inserted successfully");
             }
-        }); 
-    }else{
-        console.log("Record is not inserted");
+        });
+    } else {
         res.send("Record is not inserted");
-    }    
+    }
 });
 
-<!--------- fetch data from mongodb through url ---->
+<!--------- fetch data from mongodb through url -------->
 
-router.all('/users/create/:id', function (req, res) {
- var userid = req.params.id;
- req.Collection_user.find({
-     "_id": userid,
+router.all('/users/create/:id', function(req, res) {
+    var userid = req.params.id;
+    req.Collection_user.find({
+        "_id": userid,
     }, function(err, docs) {
-     if (err){
-        res.json("You Entered a Invalid Id");
-        console.log("You Entered a Invalid Id")
-        throw (err);
-        process.exit();
-      }else {  
-        console.log(docs);
-        res.json(docs);
-      }
-  });
+        if (err) {
+            res.json("You Entered a Invalid Id");
+        } else {
+            res.json(docs);
+        }
+    });
 });
 module.exports = router;
