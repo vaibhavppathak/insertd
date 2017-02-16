0facb5ead57bf379bcd84fb2230873d8d1956c2a
vaibhavpathak1994 <vaibhav_pathak@excellencetechnologies.in>
2017-02-16 12:34:55 +0530

    changes in index.js

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,34 @@ var express = require('express'); // require express module
 var app = express(); // creatig insatnce of express function 
 var router = express.Router();
 
+<!--- insert data into mongodb ---->
+
+router.all('/users/create', function (req, res) {
+    var name = req.body.name;
+    var email = req.body.email;
+    var password = req.body.password;
+ if(name.length > 0 && email.length > 0 && password.length > 0){
+       var record = new req.Collection_user({
+          "name": name,
+          "email": email,
+          "password":password,
+        });
+       record.save(function (err) {
+          if(err){
+               throw error;
+            } else {
+              console.log("record is inserted successfully");
+              res.send("Record inserted successfully");
+            }
+        }); 
+    }else{
+        console.log("Record is not inserted");
+        res.send("Record is not inserted");
+    }    
+});
+
+<!--------- fetch data from mongodb through url ---->
+
 router.all('/users/create/:id', function (req, res) {
  var userid = req.params.id;
  req.Collection_user.find({
