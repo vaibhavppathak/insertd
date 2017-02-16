390d21e0beac5167318d78649d27949c8494ad18
vaibhavpathak1994 <vaibhav_pathak@excellencetechnologies.in>
2017-02-16 11:32:29 +0530

    fetch data through url

--- routes/index.js
+++ routes/index.js
@@ -2,27 +2,20 @@ var express = require('express'); // require express module
 var app = express(); // creatig insatnce of express function 
 var router = express.Router();
 
-router.all('/users/create', function (req, res) {
-    var name = req.body.name;
-    var email = req.body.email;
-    var password = req.body.password;
- if(name.length > 0 && email.length > 0 && password.length > 0){
-       var record = new req.Collection_user({
-          "name": name,
-          "email": email,
-          "password":password,
-        });
-       record.save(function (err) {
-          if(err){
-               throw error;
-            } else {
-              console.log("record is inserted successfully");
-              res.send("Record inserted successfully");
-            }
-        }); 
-    }else{
-        console.log("Record is not inserted");
-        res.send("Record is not inserted");
-    }    
+router.all('/users/:id', function (req, res) {
+ console.log(req.params.id);;
+ var userid = req.params.id;
+ req.Collection_user.find({
+  "_id": userid,
+}, function(err, docs) {
+
+  if (!err) {
+    if (docs.length > 0) {
+      console.log(docs);
+      res.send(docs);
+
+    }
+  }
+});
 });
 module.exports = router;
